ed7aefd88f16b76e3dc41a303c1d891e6bfe568f
Aravind karthik <aravind.karthik@go-jek.com>
2017-04-22 16:24:53 +0530

    added serial communication and a test button

--- app/src/main/java/com/farm/bot/MainActivity.java
+++ app/src/main/java/com/farm/bot/MainActivity.java
@@ -1,13 +1,160 @@
 package com.farm.bot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.akhgupta.easylocation.EasyLocationAppCompatActivity;
+import com.akhgupta.easylocation.EasyLocationRequest;
+import com.akhgupta.easylocation.EasyLocationRequestBuilder;
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+import com.google.android.gms.location.LocationRequest;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class MainActivity extends EasyLocationAppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private UsbManager usbManager;
+    private static final String ACTION_USB_PERMISSION =
+            "com.android.example.USB_PERMISSION";
+    private UsbSerialDevice serialPort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usbManager = (UsbManager) getApplicationContext().getSystemService(USB_SERVICE);
+
+        setupUsb();
+        requestLocation();
+    }
+
+    private void requestLocation() {
+        LocationRequest locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setInterval(5000)
+                .setFastestInterval(5000);
+
+        EasyLocationRequest easyLocationRequest = new EasyLocationRequestBuilder()
+                .setLocationRequest(locationRequest)
+                .setFallBackToLastLocationTime(3000)
+                .build();
+        requestLocationUpdates(easyLocationRequest);
+
+    }
+
+    private void setupUsb() {
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (deviceIterator.hasNext()) {
+            UsbDevice device = deviceIterator.next();
+            Log.d(TAG, device.getDeviceName());
+            stringBuilder.append(device.getDeviceName() + "\n");
+            PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+            registerReceiver(mUsbReceiver, filter);
+            manager.requestPermission(device, mPermissionIntent);
+            break;
+            //your code
+        }
+        ((TextView) findViewById(R.id.helpText)).setText(stringBuilder.toString());
+
+    }
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (device != null) {
+                            UsbDeviceConnection connection = usbManager.openDevice(device);
+
+
+                            connection = usbManager.openDevice(device);
+                            serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                            if (serialPort != null) {
+                                if (serialPort.open()) { //Set Serial Connection Parameters.
+                                    serialPort.setBaudRate(9600);
+                                    serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                                    serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                                    serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                                    serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+//                                    serialPort.read(mCallback); //
+                                    Log.d(TAG,"Serial Connection Opened!\n");
+                                    String string="w";
+                                    serialPort.write(string.getBytes());
+
+
+                                } else {
+                                    Log.d("SERIAL", "PORT NOT OPEN");
+                                }
+                            } else {
+                                Log.d("SERIAL", "PORT IS NULL");
+                            }
+
+                            //call method to set up device communication
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    public void onClickTest(View view) {
+        if (serialPort != null) {
+            serialPort.write("w".getBytes());
+            serialPort.write("a".getBytes());
+            serialPort.write("s".getBytes());
+            serialPort.write("d".getBytes());
+        }
+    }
+
+    @Override
+    public void onLocationPermissionGranted() {
+
+    }
+
+    @Override
+    public void onLocationPermissionDenied() {
+
+    }
+
+    @Override
+    public void onLocationReceived(Location location) {
+        if(location!=null)
+        Log.d(TAG,location.getLatitude()+","+location.getLongitude());
+
+    }
+
+    @Override
+    public void onLocationProviderEnabled() {
+
+    }
+
+    @Override
+    public void onLocationProviderDisabled() {
+
     }
 }
