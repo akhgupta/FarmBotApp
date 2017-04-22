844a1803886465960befd657520d7dc26104f4a8
akhil <akhil.gupta@go-jek.com>
2017-04-22 12:14:31 +0530

    Initial commit

---
+++ app/src/main/java/com/farm/bot/LiveVideoActivity.java
@@ -0,0 +1,13 @@
+package com.farm.bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LiveVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_video);
+    }
+}
